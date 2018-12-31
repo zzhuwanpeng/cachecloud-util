fe18246d16d47110e534f617a50298ca9e38fdbb
zhuwp <zhuwp@kaolafm.com>
2018-12-31 21:45:44 +0800

    init run

---
+++ readme.txt
@@ -0,0 +1,1 @@
+java -jar cachecloud-util-1.0-SNAPSHOT.jar
---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by zhuwp on 18/12/31.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1111");
+    }
+}
